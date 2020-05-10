7ae04945861dd0bc6177f55c194caa89db20d7bb
Allegra Megan Anka <allegra.anka@gmail.com>
2020-05-09 21:34:29 -0400

    added header component and header styling, with logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-
-export const HatsPage = (props) => {
-  console.log(props);
-  return(
-    <>
-      <h1>hats page</h1>
-      <p>need a hat?</p>
-    </>
-  );
-} 
+import ShopPage from './pages/shop/shop.component'; 
 
 function App() {
   return (
     <>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/shop' component={ShopPage}/>
-      <Route path='/shop/hats' component={HatsPage}/>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
     </>
   );
 }
