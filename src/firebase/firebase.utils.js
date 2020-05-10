84981245c0819e505c3b5b6f69b754099d233ada
Allegra Megan Anka <allegra.anka@gmail.com>
2020-05-10 13:31:35 -0400

    firebase project set up and linked, and auth with google implemented through firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app'; // you always need to import this firebase keyword to use the utility library
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// initialize firebase credentials in app configuration
+const config = {
+    apiKey: "AIzaSyAMFNwo7Gmt1bmKrOudgfF8_ASuIwXgdTM",
+    authDomain: "danya-db.firebaseapp.com",
+    databaseURL: "https://danya-db.firebaseio.com",
+    projectId: "danya-db",
+    storageBucket: "danya-db.appspot.com",
+    messagingSenderId: "323299994882",
+    appId: "1:323299994882:web:16daa861e23616905d261b",
+    measurementId: "G-LER8L0H3PZ"
+  };
+
+  // pass specific config values into this firebase init method
+  firebase.initializeApp(config);
+
+  export const firestore = firebase.firestore();
+  export const auth = firebase.auth();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt: 'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  // in case we want access to the whole library at some point, export firebase
+  export default firebase;
