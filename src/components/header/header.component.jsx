0a9926ff844de1de3f6aeff0deee2639a84d4c96
Allegra Megan Anka <allegra.anka@gmail.com>
2020-05-16 13:23:05 -0400

    added cart reducer and logic to toggle the cart from hidden to shown in the header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,9 @@ import { ReactComponent as Logo } from '../../assets/images/logo.svg';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -32,13 +33,15 @@ const Header = ({ currentUser }) => (
             )}
             <CartIcon />
         </div>
+        { hidden ? null : <CartDropdown /> }
     </div>
-)
+);
 
 // used to grab data from the store that the connected component needs as props
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 });
 
-// connect component to the store
+// connect component to the data store - the root reducer
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 // code that combines all of our state together
 import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 });
