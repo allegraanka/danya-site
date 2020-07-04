07fa4bc953b300514e9086d51cd5e9e22732b622
Allegra Megan Anka <allegra.anka@gmail.com>
2020-07-04 00:03:33 -0400

    started adding local storage with redux-persist, persisting cart data for user sessions and across tabs

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 // code that combines all of our state together
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfiguration = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfiguration, rootReducer);
