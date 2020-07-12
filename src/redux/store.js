1641cbebd6ff7e10c32b952c658268b207aff283
Allegra Megan Anka <allegra.anka@gmail.com>
2020-07-12 14:11:58 -0400

    removed logger from production, only console logging in development

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,13 @@ import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+// set middlewares to empty so that logging to the console does not occur on production
+const middlewares = [];
+
+// only push logger into our middlewares array if we're in development, so logging only occurs in development
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 // instantiate store object, and spread in all of the values in the array into the function call as individual arguments
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
